f080264570e726e5a6946e0ab97ceb083b6cd636
Walter Baschmann <baschmann.w@gmail.com>
2024-03-13 21:48:00 -0300

    ADD: ajustes finales a js y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* cambio de color de barra de navegación */
 $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
       $('#navbar').addClass('scrolled');
@@ -5,3 +6,13 @@ $(window).scroll(function() {
       $('#navbar').removeClass('scrolled');
     }
   });
+
+/* Tooltips */
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#enviar').click(function () {
+    alert("El mensaje fue enviado correctamente!!")
+});
